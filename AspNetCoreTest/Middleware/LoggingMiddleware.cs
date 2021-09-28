b27c4b12821c36b633c4e1f3db9bb9d7823309dc
tugkanmeral <tugkan.mrl@gmail.com>
2021-09-28 23:22:41 +0300

    Add project files.

---
+++ AspNetCoreTest/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using Serilog.Context;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        ILogger _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //First, get the incoming request
+            var requestBodyString = await GetRequestBody(context.Request);
+
+            //Copy a pointer to the original response body stream
+            var originalBodyStream = context.Response.Body;
+
+            //Create a new memory stream...
+            using (var responseBody = new MemoryStream())
+            {
+                //...and use that for the temporary response body
+                context.Response.Body = responseBody;
+
+                LogContext.PushProperty("RequestBody", requestBodyString);
+
+                try
+                {
+                    //Continue down the Middleware pipeline, eventually returning to this class
+                    await _next(context);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Information("Exception : {Exception}", ex.ToString());
+                }
+
+                //Format the response from the server
+                var responseBodyString = await GetResponseBody(context.Response);
+
+                LogContext.PushProperty("ResponseBody", responseBodyString);
+
+                //Copy the contents of the new memory stream (which contains the response) to the original stream, which is then returned to the client.
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> GetRequestBody(HttpRequest request)
+        {
+            var body = request.Body;
+
+            //This line allows us to set the reader for the request back at the beginning of its stream.
+            request.EnableBuffering();
+
+            //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            //...Then we copy the entire request stream into the new buffer.
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            //We convert the byte[] into a string using UTF8 encoding...
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            request.Body = body;
+
+            return $"{request.Scheme} {request.Host} {request.Path} {request.QueryString} {bodyAsText}";
+        }
+
+        private async Task<string> GetResponseBody(HttpResponse response)
+        {
+            //We need to read the response stream from the beginning...
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //...and copy it into a string
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            //We need to reset the reader for the response so that the client can read it.
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            return $"{response.StatusCode}: {text}";
+        }
+    }
+}
---
+++ AspNetCoreTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] [{Address}] {Site}: {Message}{NewLine}ResponseBody: {ResponseBody}{NewLine}RequestBody: {RequestBody}{NewLine}")
+            .CreateBootstrapLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
